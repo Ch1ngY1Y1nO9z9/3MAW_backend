0cac55eac8424a92f545f8d54aff50eb1b9de492
Ch1ngY1Y1O9z9 <l123050891@gmail.com>
2022-05-16 22:54:17 +0800

    Update index.blade.php

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.template')
 
 @section('recaptcha')
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 @endsection
 
 @section('css')
+    <style>
 
-<style>
 
-
-</style>
-
+    </style>
 @endsection
 
 @section('content')
@@ -19,14 +17,14 @@
         <section>
             <div class="swiper-container bannerSwiper">
                 <div class="swiper-wrapper">
-                    @foreach($banners as $banner)
+                    @foreach ($banners as $banner)
                         <div class="swiper-slide">
-                            @if($banner->links)
-                                <a href="{{$banner->links}}"  target="_blank" >
-                                    <img src="{{$banner->img}}" alt="{{$banner->alt}}">
+                            @if ($banner->links)
+                                <a href="{{ $banner->links }}" target="_blank">
+                                    <img src="{{ $banner->img }}" alt="{{ $banner->alt }}">
                                 </a>
                             @else
-                                <img src="{{$banner->img}}" alt="{{$banner->alt}}">
+                                <img src="{{ $banner->img }}" alt="{{ $banner->alt }}">
                             @endif
                         </div>
                     @endforeach
@@ -52,38 +50,41 @@
         </section>
 
         <section class="body-font" id="AboutUs">
-          <div class="container px-5 md:py-24 py-10 mx-auto flex flex-wrap">
-            <div class="flex flex-wrap w-full">
-                <div class="lg:w-1/3 md:w-1/2 object-cover object-center rounded-lg">
-                    <div class="flex flex-wrap w-full mb-10">
-                        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-                            <h1 class="sm:text-3xl text-2xl font-bold title-font mb-2 text-custom-purple">About 3 Maw</h1>
-                            <div class="h-1 w-20 bg-custom-purple rounded"></div>
+            <div class="container px-5 md:py-24 py-10 mx-auto flex flex-wrap">
+                <div class="flex flex-wrap w-full">
+                    <div class="lg:w-1/3 md:w-1/2 object-cover object-center rounded-lg">
+                        <div class="flex flex-wrap w-full mb-10">
+                            <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+                                <h1 class="sm:text-3xl text-2xl font-bold title-font mb-2 text-custom-purple">About 3 Maw
+                                </h1>
+                                <div class="h-1 w-20 bg-custom-purple rounded"></div>
+                            </div>
                         </div>
+                        <img src="/img/about_us.jpg" alt="About Us">
                     </div>
-                    <img src="/img/about_us.jpg" alt="About Us">
-                </div>
-                <div class="lg:w-2/3 md:w-1/2 md:pl-10 md:py-6 font-bold text-black leading-loose md:pt-0 pt-10">
-                    <h1 class="sm:text-3xl text-2xl text-center ">Your best partner, 3Maw Rubber</h1>
-                    <h2 class="sm:text-2xl text-xl my-8 md:pl-8 pl-0">Thank you for visiting our website.</h2>
-                    <p class="tracking-wide md:text-justify md:text-lg">
-                        A specialized rubber-part manufacturer with advanced facilities and integrated production from R&D, manufacture to delivery, we supply a wide array of automotive, hardware,electronic, mechanical and healthcare equipment parts and accessories.<br>
-                        We prioritize :<br>
-                        1.	reliability<br>
-                        2.	quality<br>
-                        3.	innovation<br>
-                        4.	customer-oriented service.<br>
-                        Company operating guidelines.<br>
-                        Please contact us for further info and OEM/ODM orders.
-                    </p>
+                    <div class="lg:w-2/3 md:w-1/2 md:pl-10 md:py-6 font-bold text-black leading-loose md:pt-0 pt-10">
+                        <h1 class="sm:text-3xl text-2xl text-center ">Your best partner, 3Maw Rubber</h1>
+                        <h2 class="sm:text-2xl text-xl my-8 md:pl-8 pl-0">Thank you for visiting our website.</h2>
+                        <p class="tracking-wide md:text-justify md:text-lg">
+                            A specialized rubber-part manufacturer with advanced facilities and integrated production from
+                            R&D, manufacture to delivery, we supply a wide array of automotive, hardware,electronic,
+                            mechanical and healthcare equipment parts and accessories.<br>
+                            We prioritize :<br>
+                            1. reliability<br>
+                            2. quality<br>
+                            3. innovation<br>
+                            4. customer-oriented service.<br>
+                            Company operating guidelines.<br>
+                            Please contact us for further info and OEM/ODM orders.
+                        </p>
+
+                    </div>
 
                 </div>
-
             </div>
-          </div>
         </section>
 
-       <!-- APPLICATIONS -->
+        <!-- APPLICATIONS -->
         <section class="text-gray-600 body-font bg-custom-purple" id="Classification">
             <div class="container px-5 pt-20 pb-20 mx-auto text-white">
                 <div class="flex flex-wrap w-full mb-10">
@@ -95,7 +96,8 @@
                 <div class="flex flex-wrap -m-2">
                     <div class="p-2 lg:w-1/4 md:w-1/2 w-full text-white">
                         <div class="h-full text-center p-4 rounded-lg transform duration-300 hover:scale-105">
-                            <img class="rounded-full object-cover object-center inline-block w-64 h-64" src="/img/applications/Automotive.jpg" alt="Automotive">
+                            <img class="rounded-full object-cover object-center inline-block w-64 h-64"
+                                src="/img/applications/Automotive.jpg" alt="Automotive">
                             <div class="flex-grow">
                                 <h2 class="text-xl font-bold">Automotive</h2>
                             </div>
@@ -104,7 +106,8 @@
 
                     <div class="p-2 lg:w-1/4 md:w-1/2 w-full text-white">
                         <div class="h-full text-center p-4 rounded-lg transform duration-300 hover:scale-105">
-                            <img class="rounded-full object-cover object-center inline-block w-64 h-64" src="/img/applications/Aerospace.jpg" alt="Aerospace">
+                            <img class="rounded-full object-cover object-center inline-block w-64 h-64"
+                                src="/img/applications/Aerospace.jpg" alt="Aerospace">
                             <div class="flex-grow">
                                 <h2 class="text-xl font-bold">Aerospace</h2>
                             </div>
@@ -113,7 +116,8 @@
 
                     <div class="p-2 lg:w-1/4 md:w-1/2 w-full text-white">
                         <div class="h-full text-center p-4 rounded-lg transform duration-300 hover:scale-105">
-                            <img class="rounded-full object-cover object-center inline-block w-64 h-64" src="/img/applications/Oil&gas industry.jpg" alt="Oil&gas industry">
+                            <img class="rounded-full object-cover object-center inline-block w-64 h-64"
+                                src="/img/applications/Oil&gas industry.jpg" alt="Oil&gas industry">
                             <div class="flex-grow">
                                 <h2 class="text-xl font-bold">Oil&gas industry</h2>
                             </div>
@@ -122,7 +126,8 @@
 
                     <div class="p-2 lg:w-1/4 md:w-1/2 w-full text-white">
                         <div class="h-full text-center p-4 rounded-lg transform duration-300 hover:scale-105">
-                            <img class="rounded-full object-cover object-center inline-block w-64 h-64" src="/img/applications/Water&food industry.jpg" alt="Water&food industry">
+                            <img class="rounded-full object-cover object-center inline-block w-64 h-64"
+                                src="/img/applications/Water&food industry.jpg" alt="Water&food industry">
                             <div class="flex-grow">
                                 <h2 class="text-xl font-bold">Water&food industry</h2>
                             </div>
@@ -132,7 +137,8 @@
             </div>
         </section>
 
-        <section class="text-white body-font mt-20 bg-cover" id="News" style="background-image:url(/img/news_background.jpg)">
+        <section class="text-white body-font mt-20 bg-cover" id="News"
+            style="background-image:url(/img/news_background.jpg)">
             <div class="container px-5 py-20 mx-auto">
                 <div class="flex flex-wrap w-full mb-10">
                     <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
@@ -145,10 +151,13 @@
                         <div class="py-8 px-4">
                             <div class="h-full flex items-start">
                                 <div class="flex-grow pl-6 flex md:flex-row flex-col items-center">
-                                    <img class="rounded-full object-cover object-center inline-block w-64 h-64" src="{{$news->img}}" alt="News">
-                                    <a href="/news/{{$news->id}}" class="flex items-center md:flex-row flex-col hover:underline">
-                                        <p class="title-font text-2xl font-bold leading-relaxed mx-5">{{$news->date}}</p>
-                                        <h2 class="title-font text-2xl font-bold my-3">{{$news->title_ch}}
+                                    <img class="rounded-full object-cover object-center inline-block w-64 h-64"
+                                        src="{{ $news->img }}" alt="News">
+                                    <a href="/news/{{ $news->id }}"
+                                        class="flex items-center md:flex-row flex-col hover:underline">
+                                        <p class="title-font text-2xl font-bold leading-relaxed mx-5">{{ $news->date }}
+                                        </p>
+                                        <h2 class="title-font text-2xl font-bold my-3">{{ $news->title_ch }}
                                         </h2>
                                     </a>
                                 </div>
@@ -170,10 +179,11 @@
                 <div class="flex flex-wrap">
                     @foreach ($productTypes as $types)
                         <div class="p-4 lg:w-1/4 md:w-1/2 w-full">
-                            <a class="py-3 text-black text-center block transform duration-300 hover:scale-105 shadow" href="/Products/{{$types->id}}">
+                            <a class="py-3 text-black text-center block transform duration-300 hover:scale-105 shadow"
+                                href="/Products/{{ $types->id }}">
                                 <img class="rounded-full object-cover object-center inline-block w-56 h-56"
-                                src="{{$types->img ?? '/img/bg.png'}}" alt="{{$types->type_name_en}}">
-                                <div class="text-lg font-bold pt-3">{{$types->type_name_en}}</div>
+                                    src="{{ $types->img ?? '/img/bg.png' }}" alt="{{ $types->type_name_en }}">
+                                <div class="text-lg font-bold pt-3">{{ $types->type_name_en }}</div>
                             </a>
                         </div>
                     @endforeach
@@ -185,7 +195,8 @@
             <div class="absolute inset-0 bg-bottom bg-no-repeat" style="background-image:url('/img/contact_us.jpg')">
             </div>
             <div class="container px-5 py-32 mx-auto flex" id="ContactUs">
-                <div class="w-full bg-black bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative shadow-md">
+                <div
+                    class="w-full bg-black bg-opacity-50 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative shadow-md">
                     <div class="flex flex-wrap w-full mb-10">
                         <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
                             <h1 class="sm:text-3xl text-2xl font-bold title-font mb-2 text-white">Contact Us</h1>
@@ -236,7 +247,7 @@
 
                             </div>
                             <div class="flex-1">
-                            <div class="relative mb-4">
+                                <div class="relative mb-4">
                                     <label for="email" class="leading-7 text-sm text-white">Email</label>
                                     <input type="email" id="email" name="email" required
                                         class="w-full bg-black bg-opacity-25 rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
@@ -254,7 +265,8 @@
                                 </div>
                                 <div class="g-recaptcha" data-sitekey="6LfJjvMfAAAAAJns6znQ3dAh2QFC2ryo3h5fK9SA"></div>
                                 <button
-                        class="text-white block w-full bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit">Send</button>
+                                    class="mt-4 text-white block w-full bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+                                    type="submit">Send</button>
                             </div>
                         </div>
 
@@ -303,13 +315,11 @@
                 clickable: true,
             },
         });
-
     </script>
 
-@if(Session::has('message'))
-    <script>
-        alert('We will contact you in a few days, thank you for contacting us!')
-    </script>
-@endif
-
+    @if (Session::has('message'))
+        <script>
+            alert('We will contact you in a few days, thank you for contacting us!')
+        </script>
+    @endif
 @endsection
